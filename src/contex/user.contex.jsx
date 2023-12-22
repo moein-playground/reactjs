b4c6a7563c62715ac224e5e06a94b812fb80df50
Moein Hosseini <moein.hoseini92@gmail.com>
2023-12-22 20:11:42 +0330

    implement basic reducer to project with using of hooks - added reducer ro project using hooks useReducer in user context

--- src/contex/user.contex.jsx
+++ src/contex/user.contex.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import {
   createUserDocumentFromAuth,
   onAuthStateChangeListener,
@@ -11,10 +11,44 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
 
+const userReducer = (state, action) => {
+  console.log('dispatch');
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+export const UserProvider = ({ children }) => {
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
   const value = { currentUser, setCurrentUser };
+
+  console.log(`currentUser : ${currentUser}`);
+
   // signOutUser();
   useEffect(() => {
     const unsubscribe = onAuthStateChangeListener((user) => {
@@ -28,3 +62,12 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*
+  const userReducer = (state, action)=>{
+    return{
+      currentUser:{}
+    }
+  }
+
+*/
