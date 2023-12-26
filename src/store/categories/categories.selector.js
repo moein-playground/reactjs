ae38ab9de175cca8a153e791d90e6ff8412390bb
Moein Hosseini <moein.hoseini92@gmail.com>
2023-12-26 10:50:56 +0330

    add memorization to project

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategoiries = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories,
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategoiries],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {}),
+);
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,19 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
-// root-reducer
 import { rootReducer } from './root-reducer';
 
+const loggerMiddleWare = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log('type', action.type);
+  console.log('payload', action.payload);
+  console.log('current State', store.getState());
+
+  next(action);
+
+  console.log('next state', store.getState());
+};
 const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
